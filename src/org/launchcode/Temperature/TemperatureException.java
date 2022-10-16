3c2c6a5f8474f382c3d57317bc206e7f3f291672
Teresa <teresatull@comcast.net>
2022-10-16 04:31:46 -0500

    Done with ch 11 work (temp) and exercises 2nd save

--- src/org/launchcode/Temperature/TemperatureException.java
+++ src/org/launchcode/Temperature/TemperatureException.java
@@ -1,3 +1,4 @@
+//Teresa Tull worked on this on 10-11-2022 ch 11 work
 package org.launchcode.Temperature;
 
 public class TemperatureException extends Exception {
--- src/org/launchcode/exercises/ArithmeticException.java
+++ src/org/launchcode/exercises/ArithmeticException.java
@@ -1,3 +1,4 @@
+//Teresa Tull worked on this on 10-11-2022 ch 11 exercises
 package org.launchcode.exercises;
 
 public class ArithmeticException extends Exception {
